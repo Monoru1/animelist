5292ef0e6e4fc61ca45feead2b0d113d3cfde569
Monoru1 <162912698+Monoru1@users.noreply.github.com>
2026-05-22 23:44:23 +0200

    feat: add favorites page route

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from '@/components/layout/AppLayout'
 import { AddAnimePage } from '@/pages/AddAnimePage'
 import { AdminPage } from '@/pages/AdminPage'
 import { AnimeDetailPage } from '@/pages/AnimeDetailPage'
+import { FavoritesPage } from '@/pages/FavoritesPage'
 import { LibraryPage } from '@/pages/LibraryPage'
 import { LoginPage } from '@/pages/LoginPage'
 import { MyPlaylistPage } from '@/pages/MyPlaylistPage'
@@ -23,6 +24,7 @@ export function AppRouter() {
           <Route path="library" element={<LibraryPage />} />
           <Route path="anime/:animeId" element={<AnimeDetailPage />} />
           <Route path="history" element={<WatchHistoryPage />} />
+          <Route path="favorites" element={<FavoritesPage />} />
           <Route path="add" element={<AddAnimePage />} />
           <Route path="my-playlist" element={<MyPlaylistPage />} />
           <Route path="notifications" element={<NotificationsPage />} />
